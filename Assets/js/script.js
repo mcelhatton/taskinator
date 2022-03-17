5a74107645620d6968bc81adfc9a63395f842560
mcelhatton <colinm@mobiledealerdata.com>
2022-03-17 09:59:42 -0500

    saving task add feature

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,14 @@
-console.dir(window.document);
+// HTML pointers
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+// event listeners
+buttonEl.addEventListener("click", createTaskHandler);
+
